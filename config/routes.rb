fa3718856036af94f319c16e570c9161438ea5a8
yossepbb <b.bj@hotmail.fr>
2019-04-25 14:44:51 +0200

    add admin/session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
