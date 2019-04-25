4fd2e41a2323779036d322333c35d683eb6b42ca
yossepbb <b.bj@hotmail.fr>
2019-04-25 16:16:33 +0200

    add login, authentificate and logout pages and methods

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,11 +5,19 @@ class Admin::SessionsController < Admin::ApplicationController
   end
 
   def create
-  	@moderator = Moderator.find_by(username: params[:username]).authenticate(params[:password])
-  	session[:current_moderator_id] = @moderator.id
-  	byebug
+  	@moderator = Moderator.find_by(username: params[:username]).try(:authenticate, params[:password])
+
+  		if @moderator
+  			session[:current_moderator_id] = @moderator.id
+  			redirect_to admin_moderators_path, notice: 'You have successfull signed in'
+  		else
+  			flash[:alert] = 'There is a problem with your username or password'
+  			render 'new'
+  		end
   end
 
   def destroy
+  	session[:current_moderator_id] = nil
+  	redirect_to '/login', notice: 'You have successfull log out'
   end
 end
