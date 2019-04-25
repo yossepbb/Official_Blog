55c208893168904ea84b78ffea2278dd90833100
yossepbb <b.bj@hotmail.fr>
2019-04-25 17:19:26 +0200

    add admin/post_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
+  
  	get '/login' => 'admin/sessions#new'
  	get '/logout' => 'admin/sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
+    resources :posts
   	resources :sessions, only: [:new, :create, :destroy]
   	resources :moderators, only: [:index, :edit, :update]
   end
