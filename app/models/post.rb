a477783ad2d26004236357b85cba58762cc0055f
yossepbb <b.bj@hotmail.fr>
2019-04-26 16:58:21 +0200

    add association when delete records

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :moderator
-  has_many :comments
-  has_many :post_tags
+  has_many :comments, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
   validates :title, presence: true
   validates :content, presence: true
+
+
   
   # self bc it belongs to post's model
   def self.matching_title_or_content(search)
