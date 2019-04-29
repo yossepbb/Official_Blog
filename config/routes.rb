51217c6f26322a1d9e515772f7951f43e868db5c
yossepbb <b.bj@hotmail.fr>
2019-04-29 19:26:21 +0200

    create a link to comments index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
  	get '/login' => 'admin/sessions#new'
  	get '/logout' => 'admin/sessions#destroy'
 
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :posts
     resources :tags, except: [:index]
+    resources :comments, only: [:index, :update, :destroy]
   	resources :sessions, only: [:new, :create, :destroy]
   	resources :moderators, only: [:index, :edit, :update]
   end
