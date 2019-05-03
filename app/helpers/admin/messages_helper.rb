2db28e81c6985775db6d148639102a1af951467f
yossepbb <b.bj@hotmail.fr>
2019-05-03 16:39:20 +0200

    refacto code and create message helper's methods

--- app/helpers/admin/messages_helper.rb
+++ app/helpers/admin/messages_helper.rb
@@ -1,2 +1,16 @@
 module Admin::MessagesHelper
+
+	def message_weight(message)
+		message.status == false ? weight = 'font-weight:bold' : weight = 'font-weight:normal' 
+	end
+
+	def build_read_status_link(message)
+
+		if message.status == true
+			link_to 'Un-read', admin_message_path(message, status: false), method: :put
+		else
+			link_to 'Read', admin_message_path(message, status: true), method: :put
+		end
+		
+	end
 end
