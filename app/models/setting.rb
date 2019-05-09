9f81820653b21ec1ec994cc458439def4b06e3e5
yossepbb <b.bj@hotmail.fr>
2019-05-09 12:29:18 +0200

    catch the exception after reset DB

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,16 +2,16 @@ class Setting < ApplicationRecord
 
 	# define the name of the site regarding the moderator name
 	def self.site_name
-		Setting.first.site_name
+		Setting.first.try(:site_name)
 	end
 
 	# Define a number of post per page
 	def self.post_per_page
-		Setting.first.post_per_page
+		Setting.first.try(:post_per_page)
 	end
 
 	# ability to show tags dependent on the settings
 	def self.tag_visible?
-		Setting.first.tag_visibility
+		Setting.first.try(:tag_visibility)
 	end
 end
