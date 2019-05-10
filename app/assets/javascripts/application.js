0fc5996cfc9c949163e966c1ab9b7326ff534528
yossepbb <b.bj@hotmail.fr>
2019-05-10 12:12:18 +0200

    add bootstrap.css and botstrap.js, default.css and nav partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require rails-ujs
+//= require bootstrap.min
 //= require activestorage
 //= require turbolinks
 //= require_tree .
